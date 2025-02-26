bf9621b6cb7dfd27cc309f08ea34bf8ab22b4cb7
Paola Ivana Basualdo <paoladeembalse@gmail.com>
2025-02-26 16:27:14 +0000

    update dia2.js

--- dia2.js
+++ dia2.js
@@ -23,3 +23,19 @@ Y luego, dependiendo de la respuesta, debería mostrar uno de los siguientes men
 
 2 > Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?
 */
+
+const nombre = prompt("¿Cuál es tu nombre?");
+const edad = prompt("¿Cuántos años tienes?");
+const lenguaje = prompt("¿Qué lenguaje de programación estás estudiando?");
+let mensaje = `Hola ${nombre}, tienes ${edad} años y ya estás aprendiendo ${lenguaje}!`;
+alert(mensaje);
+const opcion = propmt(`¿Te gusta estudiar ${lenguaje}? Responde con el número 1 para SÍ o 2 para NO.`);
+while(true){ 
+    if (opcion === 1){
+    alert(`¡Muy bien! Sigue estudiando y tendrás mucho éxito.` );
+    break
+}else if (opcion === 2){
+    alert(`Oh, qué pena... ¿Ya intentaste aprender otros lenguajes?`)
+    break
+}else {
+ alert("Ingrese una opcion valida")   }}; 
